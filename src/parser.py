07e2abfaf5c7f37bd4d9be40fc6027fd92bc7b1c
Abdulaileb <abdulaileb@gmail.com>
2025-08-07 00:29:59 +0200

    created a new data folder

--- src/parser.py
+++ src/parser.py
@@ -16,21 +16,6 @@ def load_file(max_length=10, preview=True):
         
     return file[:max_length]
 
-    
-    
-
-# def read_failed_logins(log_path, max_line=2):
-    
-#     """ Read the log file for unsuccessful login attempts. """
-    
-#     with open(log_path, 'r') as f:
-#         for line in islice(f, max_line):
-#             if "Failed Password" in line:
-#                 failed_lines.append(line.strip())
-#     return failed_lines
-
-
-### A function to represent the data in a tabular form 
 
 def display_extracted_data():
     """ Display the extracted data in a tabular format. """
